b6d937fd0bcd6fd09f61e075aff3b96f43b50bed
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2021-03-19 10:02:17 +0000

    Server update

--- server/server.py
+++ server/server.py
@@ -2,18 +2,24 @@
 
 import yaml
 import sockets
+import sys
 
 def main():
-    print("> Starting the server ")
+    try:
+        print("> Starting the server ")
 
-    print("> Loading configuration file")
-    with open("config.yml", "r") as cfgfile:
-        cfg = yaml.safe_load(cfgfile)
+        print("> Loading configuration file")
+        with open("config.yml", "r") as cfgfile:
+            cfg = yaml.safe_load(cfgfile)
 
-    host = cfg["server"]["host"]
-    port = cfg["server"]["port"]
+        host = cfg["server"]["host"]
+        port = cfg["server"]["port"]
 
-    sockets.start(host,port)
+        sockets.start(host, port)
+
+    except KeyboardInterrupt:
+        print("> Server shutting down")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
